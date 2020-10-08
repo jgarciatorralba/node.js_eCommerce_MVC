a12f342ebc4b629c9d72f1bcdf328bf158fefbbe
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-08 19:25:32 +0200

    Added and exported 'APP_PORT' and 'VIEWS' in 'app-config.js'

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -1,5 +1,20 @@
+// Import native 'node.js' modules
+import path from "path";
+
+// Import dependency 'dotenv'
+import {} from "dotenv/config.js";
+
 // Application port for express
+const APP_PORT = process.env.APP_PORT;
 
 // Views folder path
+const VIEWS = path.resolve(process.cwd(), "src", "views");
 
 // Secrets
+
+
+// Export all constants
+export {
+  APP_PORT,
+  VIEWS
+};
