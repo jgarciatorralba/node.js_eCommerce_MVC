3c269e16ca0452ec61997cd7b3c4424e4aa5ba26
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-10 00:15:27 +0200

    Imported 'Image' model, added method 'getProduct'

--- src/controllers/publicController.js
+++ src/controllers/publicController.js
@@ -5,8 +5,9 @@
 // Import native 'node.js' modules
 import path from "path";
 
-// Import model methods
+// Import models and methods
 import * as Product from "./../models/Products.js";
+import * as Image from "./../models/Images.js";
 
 // Import VIEWS path
 import {
@@ -14,11 +15,24 @@ import {
 } from "./../config/app-config.js";
 
 export function index(req, res){
-  Product.get(req.con, (err, data) => {
+  Product.get(req.con, (err, products) => {
+    Image.get(req.con, (err, images) => {
+      res.render(
+        path.resolve(VIEWS, "public", "homepage"), {
+          title: "Homepage",
+          images: images,
+          products: products
+        });
+    })
+  })
+}
+
+export function getProduct(req, res){
+  Product.getById(req.con, req.params.id, (err, product) => {
     res.render(
-      path.resolve(VIEWS, "public", "homepage"),
-      { title: "Homepage", data: data }
-      );
-    console.log(data);
+      path.resolve(VIEWS, "public", "product-details"), {
+        title: "Test",
+        product: product
+      });
   })
 }
