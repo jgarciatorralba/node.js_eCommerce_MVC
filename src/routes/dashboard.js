86d90a1b2a2454ecbb233954ed69da065fb5b331
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-09 09:00:37 +0200

    Imported VIEWS constant and used path.resolve instead of direct route

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,10 +1,19 @@
+// Import native 'node.js' modules
+import path from "path";
+
 // Import dependency 'express'
 import express from "express";
 
 const router = express.Router();
 
+// Import VIEWS path
+import {
+  VIEWS
+} from "./../config/app-config.js";
+
 router.get("/", (req, res) => {
-  res.render("./dashboard/dashboard.ejs");
+  // res.render("./dashboard/dashboard.ejs");
+  res.render(path.resolve(VIEWS, "dashboard", "dashboard.ejs"));
 })
 
 export {
