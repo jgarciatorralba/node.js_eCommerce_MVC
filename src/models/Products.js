dc6e804413ce92cb53e591e3619cd75e4a1f4d4c
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-09 22:42:17 +0200

    Added comments at beginning of file

--- src/models/Products.js
+++ src/models/Products.js
@@ -1,3 +1,8 @@
+/**
+ * Methods for Model "Product", used to manipulate the table "products"
+ * in the database
+ */
+
 export function get(con, callback){
   con.query("SELECT * FROM products", callback)
 }
