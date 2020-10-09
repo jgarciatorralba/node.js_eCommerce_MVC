5c5f2e211348ae32ded6a293e60f8f1e3ac3a545
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-09 12:09:02 +0200

    Moved main entry point 'app.js' to root folder

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // Import native 'node.js' modules
 import path from "path";
-import fs from "fs";
 
 // Import dependencies
 import express from "express";
@@ -8,17 +7,13 @@ import expressLayouts from "express-ejs-layouts";
 
 // Import constants from own file 'app-config.js'
 import {
-  APP_PORT
-} from "./config/app-config.js";
+  APP_PORT, VIEWS
+} from "./src/config/app-config.js";
 
 const app = express();
 
 // EJS
 
-// Import VIEWS path
-import {
-  VIEWS
-} from "./config/app-config.js";
 // Set path to folder 'views'
 app.set('views', VIEWS);
 
@@ -32,7 +27,7 @@ app.set('layout', path.resolve(VIEWS, "public", "layout.ejs"));
 // Routes for views in 'views/public'
 import {
   router as router_public
-} from "./routes/public.js";
+} from "./src/routes/public.js";
 
 app.use("/", router_public);
 app.use("/login", router_public);
@@ -43,7 +38,7 @@ app.use("/profile", router_public);
 // Routes for views in 'views/dashboard'
 import {
   router as router_dashboard
-} from "./routes/dashboard.js";
+} from "./src/routes/dashboard.js";
 
 app.use("/dashboard", router_dashboard);
 
