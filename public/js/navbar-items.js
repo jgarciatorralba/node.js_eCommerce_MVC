e2c0936a603f8c3953caba4923d8da0c0ba51bbc
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-10 23:52:22 +0200

    Renamed file to 'navbar-items.js', added toggle functionality

---
+++ public/js/navbar-items.js
@@ -0,0 +1,14 @@
+const url = window.location.href;
+if (url.includes('profile')) {
+  document.getElementById('profile').classList.add('active');
+  document.getElementById('cart').classList.remove('active');
+  document.getElementById('logo').classList.add('text-muted');
+} else if(url.includes('cart')) {
+  document.getElementById('cart').classList.add('active');
+  document.getElementById('profile').classList.remove('active');
+  document.getElementById('logo').classList.add('text-muted');
+} else {
+  document.getElementById('profile').classList.remove('active');
+  document.getElementById('profile').classList.remove('active');
+  document.getElementById('logo').classList.remove('text-muted');
+}
