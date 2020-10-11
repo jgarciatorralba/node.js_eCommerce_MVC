ecdf28423b69ad752444ef9837a80ce7534b6c3f
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-11 11:28:30 +0200

    Added method to get products with pagination

--- src/models/Products.js
+++ src/models/Products.js
@@ -3,9 +3,18 @@
  * in the database
  */
 
+let pageSize = 10;
+
 // Get all products in the db
 export function get(con, callback){
   con.query("SELECT * FROM products", callback)
+}
+
+// Get all products in the db with pagination
+export function get(con, page, callback){
+  let offset = (page - 1) * pageSize;
+  let limit = pageSize;
+  con.query("SELECT * FROM products LIMIT ?, ?", [offset, limit], callback)
 }
 
 export function getById(con, id, callback){
