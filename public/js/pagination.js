fc5185605838ce75607c8fcae8050b41808d324e
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-11 22:49:42 +0200

    Completed functionality in 'pagination.js'

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -1,13 +1,66 @@
 $('#loadPage').on('click', () => {
-  let currentPage = $('#loadPage').data('currentpage');
-  let totalPages = $('#loadPage').data('totalpages');
-  console.log(currentPage);
-  console.log(totalPages);
+  let currentPage = parseInt($('#loadPage').attr('data-currentpage'));
+  let totalPages = parseInt($('#loadPage').attr('data-totalpages'));
+
+  $('#loadPage').attr('data-currentPage', currentPage + 1);
 
   $.ajax({
     url: '/products/' + (currentPage + 1),
     method: 'GET'
   }).done(response => {
-      console.log(response);
+    response.products.forEach(product => {
+      printCard(product, response.images);
+    })
+
+    currentPage++;
+
+    if(currentPage >= totalPages) {
+      $('#loadPage').prop('disabled', true);
+    } else {
+      $('#loadPage').attr('data-currentPage', currentPage);
+    }
   });
 })
+
+function printCard(product, images) {
+  let productImages = [];
+  images.forEach(image => {
+    if (image.product_id == product.id) {
+      productImages.push(image);
+    }
+  })
+
+  let productCard = `
+    <div class="col-md-4">
+      <div class="card mb-4 shadow-sm">
+        <img 
+          src="${productImages[0].path}" 
+          alt="" 
+          width="100%"
+        >
+        <div class="card-body border-top">
+          <b class="card-text">
+            ${product.title}
+          </b>
+          <p class="card-text mt-2">
+            ${product.description}
+          </p>
+          <div class="d-flex justify-content-between align-items-center">
+            <button 
+              type="button" 
+              class="btn btn-sm btn-outline-secondary" 
+              data-product_id="${product.id}"
+            >
+              Add to cart
+            </button>
+            <b class="text-body">
+              ${product.price} €
+            </b>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  $('.album .container .row').append(productCard);
+}
