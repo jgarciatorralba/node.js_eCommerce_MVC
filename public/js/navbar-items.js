1759cb565b5dc7dbbf78da5409908e0f2a67df12
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-11 17:59:14 +0200

    Moved cart out of collapsable item list

--- public/js/navbar-items.js
+++ public/js/navbar-items.js
@@ -2,14 +2,20 @@ const url = window.location.href;
 
 if (url.includes('profile')) {
   document.getElementById('profile').classList.add('active');
-  document.getElementById('cart').classList.remove('active');
+  document.getElementById('cart').classList.add('text-muted');
+  document.getElementById('cart').classList.add('hoverable');
   document.getElementById('logo').classList.add('text-muted');
+  document.getElementById('logo').classList.add('hoverable');
 } else if(url.includes('cart')) {
-  document.getElementById('cart').classList.add('active');
   document.getElementById('profile').classList.remove('active');
+  document.getElementById('cart').classList.remove('text-muted');
+  document.getElementById('cart').classList.remove('hoverable');
   document.getElementById('logo').classList.add('text-muted');
+  document.getElementById('logo').classList.add('hoverable');
 } else {
   document.getElementById('profile').classList.remove('active');
-  document.getElementById('profile').classList.remove('active');
+  document.getElementById('cart').classList.add('text-muted');
+  document.getElementById('cart').classList.add('hoverable');
   document.getElementById('logo').classList.remove('text-muted');
+  document.getElementById('logo').classList.remove('hoverable');
 }
