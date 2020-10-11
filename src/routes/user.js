7d54fdad995cd5639c1c8bf15ee02d946408d8c0
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-11 12:03:18 +0200

    Re-worked routes to use 'layout-user'

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,19 +13,19 @@ import {
 
 // Routes
 router.get("/login", (req, res) => {
-  res.render(path.resolve(VIEWS, "public", "login.ejs"), { title: "Login" });
+  res.render(path.resolve(VIEWS, "public", "login.ejs"), { title: "Login", layout: "./public/layouts/layout-user" });
 })
 
 router.get("/register", (req, res) => {
-  res.render(path.resolve(VIEWS, "public", "register.ejs"), { title: "Register" });
+  res.render(path.resolve(VIEWS, "public", "register.ejs"), { title: "Register", layout: "./public/layouts/layout-user" });
 })
 
 router.get("/reset", (req, res) => {
-  res.render(path.resolve(VIEWS, "public", "reset.ejs"), { title: "Reset password" });
+  res.render(path.resolve(VIEWS, "public", "reset.ejs"), { title: "Reset password", layout: "./public/layouts/layout-user" });
 })
 
 router.get("/profile", (req, res) => {
-  res.render(path.resolve(VIEWS, "public", "profile.ejs"), { title: "Profile" });
+  res.render(path.resolve(VIEWS, "public", "profile.ejs"), { title: "Profile", layout: "./public/layouts/layout-user" });
 })
 
 export {
