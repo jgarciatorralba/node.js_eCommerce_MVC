9a803760be7c5626fe42d3d4f222e51079e7ab75
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-12 23:12:52 +0200

    Moved user-related views to separate folder and updated router 'user.js'

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,19 +13,19 @@ import {
 
 // Routes
 router.get("/login", (req, res) => {
-  res.render(path.resolve(VIEWS, "public", "login.ejs"), { title: "Login", layout: "./public/layouts/layout-user" });
+  res.render(path.resolve(VIEWS, "public", "user", "login.ejs"), { title: "Login", layout: "./public/layouts/layout-user" });
 })
 
 router.get("/register", (req, res) => {
-  res.render(path.resolve(VIEWS, "public", "register.ejs"), { title: "Register", layout: "./public/layouts/layout-user" });
+  res.render(path.resolve(VIEWS, "public", "user", "register.ejs"), { title: "Register", layout: "./public/layouts/layout-user" });
 })
 
 router.get("/reset", (req, res) => {
-  res.render(path.resolve(VIEWS, "public", "reset.ejs"), { title: "Reset password", layout: "./public/layouts/layout-user" });
+  res.render(path.resolve(VIEWS, "public", "user", "reset.ejs"), { title: "Reset password", layout: "./public/layouts/layout-user" });
 })
 
 router.get("/profile", (req, res) => {
-  res.render(path.resolve(VIEWS, "public", "profile.ejs"), { title: "Profile" });
+  res.render(path.resolve(VIEWS, "public", "user", "profile.ejs"), { title: "Profile" });
 })
 
 export {
