d43f6521e5c97356c7af2abb7c20c7ad543d88b9
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-12 21:32:09 +0200

    Re-worked router 'public.js', added route 'checkout'

--- src/routes/public.js
+++ src/routes/public.js
@@ -1,6 +1,3 @@
-// Import native 'node.js' modules
-import path from "path";
-
 // Import dependency 'express'
 import express from "express";
 
@@ -10,18 +7,11 @@ const router = express.Router();
 import * as publicController from "./../controllers/publicController.js";
 
 // Routes
-// router.get("/", publicController.index);
 router.get("/", publicController.paginatedIndex);
 router.get("/products/:page", publicController.getPageContent);
-
 router.get("/product/:id", publicController.getProduct);
-
 router.get("/cart", publicController.goToCart);
-
-router.get("/checkout/:step", (req, res) => {
-  const checkoutStep = req.params.step;
-  res.send("Checkout process - step " + checkoutStep);
-})
+router.get("/checkout/:step", publicController.goToCheckout);
 
 export {
   router
