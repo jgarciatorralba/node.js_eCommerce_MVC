d443e081012c2b2d76924408b673c8d0c5cafc46
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-13 21:25:22 +0200

    Added post route post to use w/ registration form

--- src/routes/user.js
+++ src/routes/user.js
@@ -10,6 +10,7 @@ const userController = new UserController();
 // Routes
 router.get("/login", userController.goToLogin);
 router.get("/register", userController.goToRegister);
+router.post("/register", userController.newUser);
 router.get("/reset", userController.goToReset);
 router.get("/profile", userController.goToProfile);
 
