b22290da0f2a445360dfa579b5bf91747bba16e5
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-13 21:24:34 +0200

    Added methods 'constructor' and 'create' to user model

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -4,4 +4,21 @@ import {
 } from "./../config/app-config.js";
 
 // Model class
-export class UserModel {}
+export class UserModel {
+
+  constructor(){
+    this.con = con;
+  }
+
+  create(array){
+    return new Promise((resolve, reject) => {
+      this.con.query("INSERT INTO customers (fullname, email, password) VALUES (?, ?, ?)", array, (error, result) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve("New user created successfully!");
+        }
+      })
+    })
+  }
+}
