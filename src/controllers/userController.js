b0e0ca6f2a3018372f20704eba4649049e2bda68
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-13 23:09:36 +0200

    Added method 'newPassword' and helper function to generate random pass.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -48,4 +48,34 @@ export class UserController {
       );
     })
   }
+
+  async newPassword(req, res){
+    const email = req.body.resetEmail;
+    const newPassword = generateString(6);
+    const hashedPassword = await bcrypt.hash(newPassword, parseInt(SALT_ROUNDS));
+    const promise = User.getByEmail(email);
+    promise.then(results => {
+      return User.updatePassword(results[0], hashedPassword)
+    }).then(result => {
+      res.render(
+        path.resolve(VIEWS, "public", "user", "reset.ejs"), { title: "Reset password", layout: "./public/layouts/layout-user", message: result }
+      );
+      console.log(newPassword)
+    }).catch(error => {
+      res.render(
+        path.resolve(VIEWS, "public", "user", "reset.ejs"), { title: "Reset password", layout: "./public/layouts/layout-user", message: error }
+      );
+    })
+  }
+}
+
+// Helper function to generate random string
+function generateString(length) {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
