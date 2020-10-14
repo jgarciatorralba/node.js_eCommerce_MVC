6ee852edc7c1b00ecdd0b3c456b4537aaa5bcf91
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-14 19:39:39 +0200

    Re-worked logic for navbar items highlighting

--- public/js/navbar-items.js
+++ public/js/navbar-items.js
@@ -1,19 +1,25 @@
 const url = window.location.href;
 
 if (url.includes('profile')) {
-  document.getElementById('profile').classList.add('active');
+  if (document.getElementById('profile') !== null) {
+    document.getElementById('profile').classList.add('active');
+  }
   document.getElementById('cart').classList.add('text-muted');
   document.getElementById('cart').classList.add('hoverable');
   document.getElementById('logo').classList.add('text-muted');
   document.getElementById('logo').classList.add('hoverable');
 } else if(url.includes('cart') || url.includes('checkout')) {
-  document.getElementById('profile').classList.remove('active');
+  if (document.getElementById('profile') !== null) {
+    document.getElementById('profile').classList.remove('active');
+  }
   document.getElementById('cart').classList.remove('text-muted');
   document.getElementById('cart').classList.remove('hoverable');
   document.getElementById('logo').classList.add('text-muted');
   document.getElementById('logo').classList.add('hoverable');
 } else {
-  document.getElementById('profile').classList.remove('active');
+  if (document.getElementById('profile') !== null) {
+    document.getElementById('profile').classList.remove('active');
+  }
   document.getElementById('cart').classList.add('text-muted');
   document.getElementById('cart').classList.add('hoverable');
   document.getElementById('logo').classList.remove('text-muted');
