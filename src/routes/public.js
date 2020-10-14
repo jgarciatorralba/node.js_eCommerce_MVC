798efd0f7ced299ee7dcc2b28e00356e8c0510ca
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-14 16:53:51 +0200

    Added route protection with authentication middleware

--- src/routes/public.js
+++ src/routes/public.js
@@ -7,11 +7,14 @@ const router = express.Router();
 import { PublicController } from "./../controllers/publicController.js";
 const publicController = new PublicController();
 
+// Import own middleware "auth.js"
+import { ensureAuthenticated } from "../config/auth.js";
+
 // Routes
 router.get("/", publicController.paginatedIndex);
 router.get("/products/:page", publicController.getPageContent);
 router.get("/product/:id", publicController.getProduct);
-router.get("/cart", publicController.goToCart);
+router.get("/cart", ensureAuthenticated, publicController.goToCart);
 router.get("/checkout/:step", publicController.goToCheckout);
 
 export {
