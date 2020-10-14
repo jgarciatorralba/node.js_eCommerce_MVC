f84bcdcd614a0a8d74b37d1ad20965b035113006
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-14 18:40:47 +0200

    Re-arranged routes in router 'user.js'

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,12 +13,16 @@ const Auth = new AuthUtil();
 
 // Routes
 router.get("/login", Auth.ensureNotAuthenticated, userController.goToLogin);
+router.post("/login", userController.authenticate);
+
 router.get("/register", userController.goToRegister);
 router.post("/register", userController.newUser);
+
 router.get("/reset", userController.goToReset);
 router.post("/reset", userController.newPassword);
+
 router.get("/profile", userController.goToProfile);
-router.post("/login", userController.authenticate);
+
 router.get("/logout", userController.logout);
 
 export {
