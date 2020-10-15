70325b4400ca5659bd5e86b2345885eec43b45c4
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-15 17:54:26 +0200

    Added csrf protection in routers

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,11 @@
 // Import dependency 'express'
 import express from "express";
+// Import csrf
+import csrf from "csurf";
+const csrfProtection = csrf({cookie: true});
 
 const router = express.Router();
+router.use(csrfProtection);
 
 // Import controller methods
 import { UserController } from "./../controllers/userController.js";
