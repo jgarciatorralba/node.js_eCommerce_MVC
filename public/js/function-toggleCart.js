75be34ed491b26488727ec29bdfb29815898af92
jgarciatorralba <jgarciatorralba@gmail.com>
2020-10-17 00:52:44 +0200

    Added increase/decrease of stock in product details

--- public/js/function-toggleCart.js
+++ public/js/function-toggleCart.js
@@ -32,11 +32,19 @@ function toggleCart(){
       $(this).text('Remove');
       $(this).removeClass('btn-outline-info');
       $(this).addClass('btn-outline-danger');
+      if ($('#product-detail-stock') !== null) {
+        let stock = parseInt($('#product-detail-stock').text())
+        $('#product-detail-stock').text(stock - 1)
+      }
     } else if (response.messageCart == "Product removed from customer cart") {
       $('#productCount').text(productCount - 1);
       $(this).text('Add to cart');
       $(this).removeClass('btn-outline-danger');
       $(this).addClass('btn-outline-info');
+      if ($('#product-detail-stock') !== null) {
+        let stock = parseInt($('#product-detail-stock').text())
+        $('#product-detail-stock').text(stock + 1)
+      }
     }
   });
 }
